c6c440bdbf9c827566ebae899e679fac2e3bea56
hadeshades007 <as2193@exeter.ac.uk>
2025-03-25 03:17:18 +0000

    commit 5

--- practice1.py
+++ practice1.py
@@ -1,4 +1,10 @@
 import pandas as pd
-data = [10, 20, 30, 40]
-series = pd.Series(data)
-print(series)
+import numpy as np
+data = {
+    "Name": ["Alice", "Bob", "Charlie"],
+    "Age": [25, 30, 35],
+    "Salary": [50000, 60000, 70000]
+}
+df = pd.DataFrame(data)
+print(df)
+print(df['Name'])
