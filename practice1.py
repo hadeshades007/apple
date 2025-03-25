2c91e74a27320637e3f6b1ddbf6cd4b9dd9c44ec
hadeshades007 <as2193@exeter.ac.uk>
2025-03-25 04:08:14 +0000

    commit

--- practice1.py
+++ practice1.py
@@ -1,10 +1,9 @@
+# Importing pandas and numpy
 import pandas as pd
 import numpy as np
-data = {
-    "Name": ["Alice", "Bob", "Charlie"],
-    "Age": [25, 30, 35],
-    "Salary": [50000, 60000, 70000]
-}
-df = pd.DataFrame(data)
-print(df)
-print(df['Name'])
+
+# Load the CSV file
+df = pd.read_csv("employee_data.csv")
+
+# Display the first 5 rows
+print(df.head())
